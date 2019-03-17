852071f7ebacc4d667bef4adc183f1195bfa20e8
ChiefChippy2 <45596322+ChiefChippy2@users.noreply.github.com>
2019-03-17 16:35:14 +0100

    Update notepad.js

--- notepad.js
+++ notepad.js
@@ -1,3 +1,13 @@
 //Uhhh
+var ss={
+"get" :function(key) {LocalStorage.getItem(key)}, 
+
+
+
+
+"set" :function(key,val) {LocalStorage.setItem(key,val)} 
+} 
 function(){}
 alert('wait') 
+ss.set('test','543')
+alert(ss.get('test'))
